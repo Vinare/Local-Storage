c0d713feb5240cd018b9dbef9b4008a689dfa220
Vinare <scrat-me@mail.ru>
2022-02-14 18:37:41 +0300

    Almost done

---
+++ js/script.js
@@ -0,0 +1,54 @@
+'use strict';
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+const toDoData = [];
+
+const render = function() {  // интегрирует новые li вместо объектов массива
+  todoList.innerHTML = '',  // обнуляем массив
+  todoCompleted.innerHTML = '',
+
+  toDoData.forEach(function(item) { // вставляем li в нужный список
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+      '<div class="todo-buttons">' +
+        '<button class="todo-remove"></button>' +
+        '<button class="todo-complete"></button>' +
+      '</div>';
+    
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    li.querySelector('.todo-complete').addEventListener('click', function() {
+      item.completed = !item.completed;
+      render();
+    });
+
+    li.querySelector('.todo-remove').addEventListener('click', function() {
+       
+    });
+  });
+};
+
+todoControl.addEventListener('submit', function(event) {  // пушим объект с задачей в конец массива
+  event.preventDefault();
+
+  const newToDo = {
+    text: headerInput.value,
+    completed: false
+  };
+
+  if (newToDo.text !== '') { 
+  toDoData.push(newToDo);
+  headerInput.value = '';
+  } 
+  render();
+});
+
+// https://myrusakov.ru/javascript-save-array-or-object-localstorage.html
